379e250ac4ead842777b06c336f182633e61b430
safe <safezpa@126.com>
2017-07-03 16:42:03 +0800

    host change

--- Hello/settings.py
+++ Hello/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'hello',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'iouoi.cn',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
